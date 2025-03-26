1c4aa7df807dfe331e844370f4f8af350fc1ba50
andrebbs <andre.bbs@gmail.com>
2025-03-26 00:37:08 -0300

    Remove deprecated Kubernetes config map for fastfoodapi

--- k8s-config.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_config_map" "fastfoodapi_config" {
-  metadata {
-    name = "fastfoodapi-config"
-  }
-
-  data = {
-    DatabaseConnectionString = "Host=terraform-20250325053356934400000001.cc66avwpkeh5.us-east-1.rds.amazonaws.com;Port=5432;Username=fastfood;Password=adminpassword123;Database=fastfooddb"
-  }
-}
