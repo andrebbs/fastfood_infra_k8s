dcef65dbfab76903396c91c126569bb5900af204
andrebbs <andre.bbs@gmail.com>
2025-03-25 22:18:00 -0300

    Refactor EKS cluster configuration for improved readability and add lifecycle management

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -3,11 +3,15 @@ resource "aws_eks_cluster" "fastfood" {
   role_arn = var.labrole
 
   vpc_config {
-    subnet_ids         = ["${var.subnetA}", "${var.subnetB}", "${var.subnetC}"]
-    security_group_ids = ["${var.sgId}"]
+    subnet_ids         = [var.subnetA, var.subnetB, var.subnetC]
+    security_group_ids = [var.sgId]
   }
 
   access_config {
     authentication_mode = var.accessConfig
+  }
+
+  lifecycle {
+    ignore_changes = [name]
   }
 }
